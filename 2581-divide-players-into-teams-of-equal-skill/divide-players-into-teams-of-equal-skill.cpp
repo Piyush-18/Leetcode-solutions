f76b154d15c402cf59dcdc1f50c24a129b35ab60
Piyush Pankaj Panigrahi <95301249+Piyush-18@users.noreply.github.com>
2024-10-04 08:44:50 +0530

    Time: 42 ms (99.60%) | Memory: 55.8 MB (98.40%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
-        int total = accumulate(skill.begin(), skill.end(), 0);
         int n = skill.size();
-        if (n % 2 != 0) return -1;  // Can't divide if odd number of players
-        int teams = n / 2;
-        if (total % teams != 0) return -1;  // Chemistry not divisible evenly
-        int chemistry = total / teams;
-        
-        unordered_map<int, int> hash;
-        int count=0;
-        long long sum = 0;
-        for (int i = 0; i < n; i++) {
-            int rem = chemistry - skill[i];
-            if (hash[rem] > 0) {
-                sum += (long long)(skill[i]*rem);
-                count++;
-                hash[rem]--;
-            } else {
-                hash[skill[i]]++;
-            }
+
+        vector<int> vec(1001, 0);
+        int SUM = 0;
+        for(int &s : skill) {
+            SUM += s;
+            vec[s]++; //freq updated
         }
-        
-        // If not enough valid pairs are formed, return -1
-        if (count != teams) return -1;
-        return sum;
+
+        int teams = n/2;
+
+        if(SUM % teams != 0) {
+            return -1;
+        }
+
+        int target = SUM / teams;
+        long long chem = 0;
+
+        for(int i = 0; i < n; i++) {
+            int currSkill   = skill[i];
+            int remainSkill = target - currSkill;
+
+            if(vec[remainSkill] <= 0) {
+                return -1;
+            }
+
+            chem += (long long)currSkill * (long long)(remainSkill);
+            vec[remainSkill] -= 1;
+        }
+
+        return chem/2;
+
     }
 };

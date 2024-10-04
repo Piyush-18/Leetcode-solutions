3be487dc03b3a82846c69630f84ace7df9307c22
Piyush Pankaj Panigrahi <95301249+Piyush-18@users.noreply.github.com>
2024-10-04 08:28:20 +0530

    Time: 66 ms (46.60%) | Memory: 56.8 MB (20.40%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -9,12 +9,13 @@ public:
         int chemistry = total / teams;
         
         unordered_map<int, int> hash;
-        vector<pair<int, int>> team_pairs;
-        
+        int count=0;
+        long long sum = 0;
         for (int i = 0; i < n; i++) {
             int rem = chemistry - skill[i];
             if (hash[rem] > 0) {
-                team_pairs.push_back({skill[i], rem});
+                sum += (long long)(skill[i]*rem);
+                count++;
                 hash[rem]--;
             } else {
                 hash[skill[i]]++;
@@ -22,13 +23,7 @@ public:
         }
         
         // If not enough valid pairs are formed, return -1
-        if (team_pairs.size() != teams) return -1;
-
-        long long sum = 0;
-        for (int i = 0; i < teams; i++) {
-            sum += (long long)team_pairs[i].first * team_pairs[i].second;
-        }
-        
+        if (count != teams) return -1;
         return sum;
     }
 };
